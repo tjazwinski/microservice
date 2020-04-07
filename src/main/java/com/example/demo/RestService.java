36f4c62eb5b5a40cc76c80b8cc858813a8dfc19a
Tomasz Jazwinski <tomaszjazwinski@wp.pl>
2020-04-07 15:09:07 +0200

    ok

---
+++ src/main/java/com/example/demo/RestService.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class RestService {
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello world";
+	}
+}
