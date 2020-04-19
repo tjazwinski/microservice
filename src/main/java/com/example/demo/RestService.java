ee58b6b975ab3a01e2af3e8f6c0e8465b7def7f1
Tomasz Jazwinski <tomaszjazwinski@wp.pl>
2020-04-19 14:55:02 +0200

    jps

--- src/main/java/com/example/demo/RestService.java
+++ src/main/java/com/example/demo/RestService.java
@@ -1,20 +1,46 @@
 package com.example.demo;
 
-import java.util.logging.Level;
+import java.util.List;
 import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.Task;
+import com.example.demo.jpa.TaskRepository;
+
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/tasks")
 public class RestService {
 
 	Logger logger = Logger.getLogger(RestService.class.getCanonicalName());
 
-	@RequestMapping("/hello")
-	public String hello() {
-		logger.log(Level.INFO, "hello world");
-		return "hello world2";
+	@Autowired
+	private TaskRepository taskRepository;
+
+	@GetMapping("/list/{userId}")
+	public List<Task> getTasks(@PathVariable Long userId) {
+		return taskRepository.findByUserId(userId);
+	}
+
+	@GetMapping("/task/{id}")
+	public Task getTask(@PathVariable Long id) {
+		return taskRepository.findById(id).get();
+	}
+
+	@PostMapping("/task")
+	public List<Task> addTask(Task task) {
+		taskRepository.save(task);
+		return getTasks(task.getUserId());
+	}
+
+	@PutMapping("/task")
+	public void setDone(Long id) {
+		taskRepository.setDone(id);
 	}
 }
